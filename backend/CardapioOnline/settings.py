0ec0381aaab00ff485699cfc4eb2cf0518994759
João Pedro da Silva Bertelli <109303186+JotapBertelli@users.noreply.github.com>
2025-09-17 15:47:09 -0300

    Add files via upload

--- backend/CardapioOnline/settings.py
+++ backend/CardapioOnline/settings.py
@@ -47,12 +47,8 @@ WSGI_APPLICATION = "CardapioOnline.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "cardapio_db",
-        "USER": "admin",
-        "PASSWORD": "admin@admin",
-        "HOST": "localhost",
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
--- backend/CardapioOnline/urls.py
+++ backend/CardapioOnline/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("cardapio.urls")),
+    path("admin/", admin.site.urls),          # painel do Django Admin
+    path("api/", include("cardapio.urls")),   # suas rotas da app 'cardapio'
 ]
